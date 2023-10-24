505d6939e6e649e20e258cd2ee6b68873c9fee6a
antx <7877940+antx-code@users.noreply.github.com>
2023-10-24 20:51:03 +0800

    init project

--- lua/config/enviroments.lua
+++ lua/config/enviroments.lua
@@ -1,1 +1,10 @@
-vim.g.python3_host_prog = "/home/soc/anaconda3/bin/python3"
+local handle = io.popen("uname")
+local result = handle:read("*a")
+handle:close()
+os_name = string.lower(result)
+
+if os_name:match("linux") then
+	vim.g.python3_host_prog = "/home/soc/anaconda3/bin/python3"
+elseif os_name:match("darwin") then -- Mac OS X uses Darwin
+	vim.g.python3_host_prog = "/Users/antx/.local/pipx/venvs/poetry/bin/python3"
+end
