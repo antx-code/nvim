f8e298d05775bbf9686b0951eabeef44e3d8fc68
antx-code <7877940+antx-code@users.noreply.github.com>
2024-08-21 18:06:55 +0800

    fix dependence

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,17 +3,37 @@
 -- 另外，config键对应的值是一个函数。这个函数使用了vim.fn["mkdp#util#install"]()来安装Markdown预览所需的工具。
 -- 总的来说，这个文件的功能是配置Markdown预览插件，并定义了一些可用的命令和安装所需工具的函数。
 
+-- return {
+--   {
+--     "iamcco/markdown-preview.nvim",
+--     cmd = {
+--       "MarkdownPreview",
+--       "MarkdownPreviewStop",
+--       "MarkdownPreviewToggle",
+--     },
+--     ft = { "markdown" },
+--     config = function()
+--       vim.fn["mkdp#util#install"]()
+--     end,
+--   },
+-- }
+
 return {
   {
+    -- Install markdown preview, use npx if available.
     "iamcco/markdown-preview.nvim",
-    cmd = {
-      "MarkdownPreview",
-      "MarkdownPreviewStop",
-      "MarkdownPreviewToggle",
-    },
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    config = function()
-      vim.fn["mkdp#util#install"]()
+    build = function(plugin)
+      if vim.fn.executable "npx" then
+        vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+      else
+        vim.cmd [[Lazy load markdown-preview.nvim]]
+        vim.fn["mkdp#util#install"]()
+      end
+    end,
+    init = function()
+      if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
     end,
   },
 }
